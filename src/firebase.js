d0eb582dfcfb1f346bdcee6f1126d7a5dcb1071f
Deeksha Tiwari <deeksha2021@gmail.com>
2021-02-04 10:39:31 +0530

    setup the netflix clone app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBBvrB9EJ0Eb6xqIjc0nMzUUN0-YJZyqoM",
+  authDomain: "netflix-clone-deeksha.firebaseapp.com",
+  projectId: "netflix-clone-deeksha",
+  storageBucket: "netflix-clone-deeksha.appspot.com",
+  messagingSenderId: "1049245313655",
+  appId: "1:1049245313655:web:4c2945c6f63169ddffb472",
+  measurementId: "G-BW7DEDPZML",
+};
