8f5e7ab811f718a0f993e009402971f45c15065d
Deeksha Tiwari <deeksha2021@gmail.com>
2021-02-04 19:24:45 +0530

    added truncate function to banner description

--- src/Banner.js
+++ src/Banner.js
@@ -2,20 +2,40 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "... " : string;
+  }
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
+        backgroundImage: `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
         <h1 className="banner__title">Movie Name</h1>
-        <button className="banner__button">Play</button>
-        <button className="banner__button">My List</button>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(
+            `This is a test description.This is a test description.This is a test
+          description.This is a test description.This is a test description.This
+          is a test description.This is a test description.This is a test
+          description.This is a test description.This is a test description.This
+          is a test description.This is a test description.This is a test
+          description.This is a test description.This is a test description.This
+          is a test description.This is a test description.This is a test
+          description.This is a test description.This is a test description.This
+          is a test description.`,
+            100
+          )}
+        </h1>
       </div>
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
